503eff71e22cc6167f166906c6f713c0ff314c29
Sjacuru <sjacuru@gmail.com>
2025-08-09 00:53:19 +1200

    up

--- PSE/Week2/Anticipations/BankAccount.py
+++ PSE/Week2/Anticipations/BankAccount.py
@@ -8,6 +8,11 @@ def creatingClass():
             self.accountNumber = accountNumber                  # Instance self . parameter 1
             self.balance = balance                              # Instance self . parameter 2
             self.ownerName = ownerName                          # Instance self . parameter 3
+
+        def __str__(self):
+            return      f"Account: {newAccount.accountNumber},\
+                        Balance: {newAccount.balance},\
+                        Name: {newAccount.ownerName}, \n"
         
         def client (self):                                      # Method that belong to the 
                                                                 #   class
@@ -16,11 +21,6 @@ def creatingClass():
                                         self.ownerName)
         def account (self):
             print("(Def account)Your account is ", self.accountNumber,"\n")
-        
-        def __str__(self):
-            return      f"Account: {newAccount.accountNumber},\
-                        Balance: {newAccount.balance},\
-                        Name: {newAccount.ownerName}, \n"
         
         def deposit(self):
             if amount > 0:
@@ -43,11 +43,12 @@ def creatingClass():
            newAccount.ownerName, "\n")                          # Use object attributes 
     print("Print class bankAcc", bankAcc, "\n")
     print("Print variable newAccount", newAccount, "\n")                                           
+    print("-------")   
     newAccount.client()
     print("-------")                                            # Use method
     newAccount.account()                                        # Use method
     
-    # Enter a 
+    
     transaction = int(input("For deposit enter 1, for withdram enter 2: "))
     if transaction == 1:
         amount = int(input("Enter the deposit value: "))
@@ -59,8 +60,6 @@ def creatingClass():
     
     else:
         print("Enter a valid number")
-
-    
 
 if __name__ == "__main__":
     ans = creatingClass()
