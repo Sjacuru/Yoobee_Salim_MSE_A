9d74c2f0a76e435228b39b38c69f0a3cac4895ac
Sjacuru <sjacuru@gmail.com>
2025-08-31 09:59:15 +1200

    Update Encapsulation.py

--- PSE/Week5/Encapsulation.py
+++ PSE/Week5/Encapsulation.py
@@ -11,6 +11,13 @@ class Student:
     def get_private(self):
         return self.__private
 
+class International(Student):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+    
+    def get_age(self):
+        return self._age
+    
 class lecturer:
     def __init__(self, lecName, lecAge):
         self.lecName = lecName
@@ -28,11 +35,15 @@ def main():
     print("Grade: " + str(s.get_grade()))
     print("Private: " + str(s.get_private()))
 
+
+    I = International("Henry", 11)
+    print("Name: " + I.name + " Age: " + str(I.get_age()))
+
+
     l = lecturer("Rao", 20)
     print("Lecturer name " + l.lecName + " Age " + str(l._lecAge))
     
     print("IRD Number: " + l.get_irdNumber())
-
 
 if __name__ == "__main__":
     main()
