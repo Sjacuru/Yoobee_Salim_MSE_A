adcea14f1b1d65a0a7422b780cad970c6b06378c
Sjacuru <sjacuru@gmail.com>
2025-10-18 11:50:04 +1300

    Lili

---
+++ PSE/Week12/Activity1.1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_flask():
+    return "<p>Activity 1. Hello, Flask!</p>"
+
+@app.route("/lili")
+def hello_lili():
+    return "<p>Hello, Lili!</p>"
+
+if __name__ == '__main__':
+    app.run(debug = True)
