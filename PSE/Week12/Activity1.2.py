f8c1507a767f54aed3d15049c593d3bf0d1afa4c
Sjacuru <sjacuru@gmail.com>
2025-10-18 11:53:29 +1300

    Lili

--- PSE/Week12/Activity1.2.py
+++ PSE/Week12/Activity1.2.py
@@ -9,5 +9,11 @@ def hello_flask():
 def hello_lili():
     return "<p>Hello, Lili!</p>"
 
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f"The square of {number} is {number**2}"
+
 if __name__ == '__main__':
     app.run(debug = True)
+
+
