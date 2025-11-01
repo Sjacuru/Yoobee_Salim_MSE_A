85e175098762c5da17cd741b573ccdc5c1776e23
Sjacuru <sjacuru@gmail.com>
2025-11-02 08:50:30 +1300

    week 14

--- PSE/Week14/week14Activ1.py
+++ PSE/Week14/week14Activ1.py
@@ -5,15 +5,20 @@ argument, returning a new function extending its functionality.
 '''
 
 def log_decorator(func):
+    print('Step 1')
     def wrapper(*args, **kwargs):
+        print('Step 2')
         print(f"Calling {func.__name__} with {args}, {kwargs}")
+        print('Step 3')
         result = func(*args, **kwargs)
         print(f"{func.__name__} returned {result}")
         return result
+    print('Step 4')
     return wrapper
     
 @log_decorator
 def add(a, b):
+        print('Step 5')
         return a + b
         
 def main():
