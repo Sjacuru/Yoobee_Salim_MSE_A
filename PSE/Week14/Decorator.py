f1838ad16f1611e6ea4bf699a8522e81ca04ac18
Sjacuru <sjacuru@gmail.com>
2025-11-02 08:31:38 +1300

    week 14

---
+++ PSE/Week14/Decorator.py
@@ -0,0 +1,20 @@
+
+'''
+THe decorator takes a function that takes another function as an 
+argument, returning a new function preserving the code.
+'''
+
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with {args}, {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned {result}")
+        return result
+    return wrapper
+    
+@log_decorator
+def add(a, b):
+        return a + b
+        
+add(3, 5)
+
